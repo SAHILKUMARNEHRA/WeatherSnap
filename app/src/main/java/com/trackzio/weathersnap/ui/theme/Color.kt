6ae18d1245c5d8ddd2a58fa00e79c7ce57d9e3b6
SAHILKUMARNEHRA <sahilkumarnehra@users.noreply.github.com>
2026-05-13 02:45:45 +0530

    Update theme to navy and beige

--- app/src/main/java/com/trackzio/weathersnap/ui/theme/Color.kt
+++ app/src/main/java/com/trackzio/weathersnap/ui/theme/Color.kt
@@ -2,18 +2,17 @@ package com.trackzio.weathersnap.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Olive950 = Color(0xFF0F120B)
-val Olive900 = Color(0xFF15190F)
-val Olive850 = Color(0xFF1B1F14)
-val Olive800 = Color(0xFF23281A)
+val Olive950 = Color(0xFF071326)
+val Olive900 = Color(0xFF0B1B33)
+val Olive850 = Color(0xFF10203D)
+val Olive800 = Color(0xFF15264A)
 
-val Mint100 = Color(0xFFDCE8D2)
-val Mint200 = Color(0xFFB8C78A)
-val Mint250 = Color(0xFFAFC47A)
+val Mint100 = Color(0xFFF4EBDD)
+val Mint200 = Color(0xFFE2D1B2)
+val Mint250 = Color(0xFFD8C29A)
 
-val CardSurface = Color(0xFF2A2E23)
-val CardBorder = Color(0xFF3A402D)
+val CardSurface = Color(0xFF0F2444)
+val CardBorder = Color(0xFF203A63)
 
 val BlueAccent = Color(0xFF5FA8FF)
 val OrangeAccent = Color(0xFFFFB85C)
-
